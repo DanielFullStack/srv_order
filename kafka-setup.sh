8d0d29b1c30fafc8774e9476cf459d1eb9320c32
Daniel Pereira do Nascimento <daniel.nascimento@etipi.pi.gov.br>
2024-11-23 01:26:04 -0300

    feat: Adicionando kafka setup

--- kafka-setup.sh
+++ kafka-setup.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-# Aguarda o Kafka iniciar
-echo "Aguardando Kafka iniciar..."
-sleep 10
-
-# Variáveis
-KAFKA_BROKER="kafka:9092"
+KAFKA_BROKER=${KAFKA_BROKER:-kafka:9092}
 TOPICS=("pedido-topic" "pedido-processado-topic" "pedido-cancelado-topic")
 
-# Cria os tópicos necessários
-for TOPIC in "${TOPICS[@]}"; do
-  echo "Criando tópico: $TOPIC"
-  kafka-topics --create \
-    --bootstrap-server $KAFKA_BROKER \
-    --replication-factor 1 \
-    --partitions 1 \
-    --topic $TOPIC \
-    || echo "Tópico $TOPIC já existe."
-done
+# Função para verificar se o Kafka está pronto
+function wait_for_kafka() {
+  echo "Esperando o Kafka inicializar em $KAFKA_BROKER..."
+  while ! nc -z kafka 9092; do
+    sleep 2
+    echo "Ainda esperando o Kafka..."
+  done
+  echo "Kafka está pronto!"
+}
 
-# Enviar mensagens de exemplo para o tópico de pedidos
-echo "Populando tópico 'pedido-topic' com mensagens de exemplo..."
-kafka-console-producer --bootstrap-server $KAFKA_BROKER --topic pedido-topic <<EOF
+# Criar tópicos no Kafka
+function create_topics() {
+  for TOPIC in "${TOPICS[@]}"; do
+    echo "Criando tópico: $TOPIC"
+    kafka-topics --create \
+      --bootstrap-server $KAFKA_BROKER \
+      --replication-factor 1 \
+      --partitions 1 \
+      --topic $TOPIC \
+      || echo "Tópico $TOPIC já existe."
+  done
+}
+
+# Enviar mensagens para o tópico de pedidos
+function populate_topic() {
+  echo "Populando tópico 'pedido-topic' com mensagens de exemplo..."
+  kafka-console-producer --bootstrap-server $KAFKA_BROKER --topic pedido-topic <<EOF
 {"produtos":[{"nome":"Notebook","preco":5000.0},{"nome":"Mouse","preco":200.0}],"status":"RECEBIDO","valorTotal":5200.0}
 {"produtos":[{"nome":"Teclado","preco":300.0},{"nome":"Monitor","preco":1500.0}],"status":"RECEBIDO","valorTotal":1800.0}
 {"produtos":[{"nome":"Smartphone","preco":2500.0},{"nome":"Carregador","preco":100.0},{"nome":"Fone de Ouvido","preco":150.0}],"status":"RECEBIDO","valorTotal":2750.0}
@@ -33,5 +41,11 @@ kafka-console-producer --bootstrap-server $KAFKA_BROKER --topic pedido-topic <<E
 {"produtos":[{"nome":"Placa de Vídeo","preco":3500.0},{"nome":"Fonte de Alimentação","preco":450.0}],"status":"RECEBIDO","valorTotal":3950.0}
 {"produtos":[{"nome":"SSD","preco":500.0},{"nome":"Memória RAM","preco":350.0}],"status":"RECEBIDO","valorTotal":850.0}
 EOF
+  echo "Mensagens enviadas para o tópico 'pedido-topic'."
+}
 
-echo "Configuração concluída."
+# Execução
+wait_for_kafka
+create_topics
+populate_topic
+echo "Configuração do Kafka concluída."
